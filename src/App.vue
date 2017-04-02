87ffe571daaa3e412b2f763e50cdd5c9183371e5
Tim Pütz <puetz_tim@web.de>
2017-04-02 18:32:06 +0200

    Completed UI
    # UX
       - Added loading animation (Spinning) while waiting for a game
       - Added developer button to emulate a found game
       - Added the tictactoe field + responsiv
    
    Testet on Chrome Desktop and Mobile

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
       </md-whiteframe>
 
       <md-list>
-        <md-list-item v-for="(game, index) in client.Games" :key="game">
+        <md-list-item v-for="(game, index) in games" :key="game">
           <i  :class="cssWinner(game)" class="md-icon mdi mdi-trophy mdi-24px"></i> <span> Gegen {{game.enemyname}}</span>
         </md-list-item>
       </md-list>
@@ -37,39 +37,136 @@
 
     <!-- <md-layout md-flex="25"></md-layout> -->
 
-    <md-layout id="searchbox" md-tag="md-card" md-gutter="40" md-column class="main-content" md-flex="50" md-align="center">
-      <md-layout md-tag="md-card-content" md-flex="100">
-        <md-input-container >
-          <md-input v-model="client.Playername" type="text" placeholder="Spielername">
+    <md-layout md-gutter="40" md-column md-flex="100" md-align="center" class="main-content">
+      <md-layout id="searchbox" v-show="!enemyFound" md-tag="md-card" md-gutter="40" md-column md-flex="50" md-align="center">
+        <md-layout md-tag="md-card-content" md-flex="100">
+          <md-input-container @:click="handleSearchGamePressed()">
+            <md-input  v-model="playername" :disabled="inQue || enemyFound" type="text" placeholder="Spielername">
 
-          </md-input>
-        </md-input-container>
+            </md-input>
+          </md-input-container>
+        </md-layout>
+        <md-layout  class="space-between" md-row md-tag="md-card-actions" md-flex="100">
+          <md-button class="md-raised bg-clouds" :disabled="inQue">
+            <div class="" v-on:click="handleSearchGamePressed()">
+              Suche Spiel...
+            </div>
+          </md-button>
+          <md-button class="md-raised bg-clouds" :disabled="!inQue">
+            <div class="" v-on:click="fakeEnemyFound()">
+              DEBUG: Spiel gefunden
+            </div>
+          </md-button>
+        </md-layout>
       </md-layout>
-      <md-layout class="space-between" md-row md-tag="md-card-actions" md-flex="100">
-        <!-- <md-button v-on:click="client.addPlayer" class="md-raised bg-clouds" >Suche Spiel...</md-button> -->
-        <button type="button" v-on:click="client.addPlayer" class="md-button md-raised bg-clouds md-theme-default"> Suche Spiel...
-          <div class="md-ink-ripple">
-            <div class="md-ripple md-fadeout md-active" style="width: 131px; height: 131px; top: -58.5px; left: -33.5px;"></div>
+      <md-layout v-if="inQue" id="loader" md-column md-align="center">
+        <md-spinner :md-size="150" :md-indeterminate="inQue || enemyFound" >
+        </md-spinner>
+        <h3 class="md-Title"> Suche Gegner ....</h3>
+      </md-layout>
+
+
+      <md-layout id="MatchPlayers" v-if="enemyFound && !inQue" md-row md-align="center">
+        <h1 class="md-display-2">{{playername}} vs. {{enemyname}}</h1>
+      </md-layout>
+
+      <md-layout v-if="enemyFound && !inQue" md-column class="align-items-center" md-align="center">
+      <!-- <md-layout md-column class="align-items-center" md-align="center"> -->
+        <md-layout id="playground" md-row md-flex="50" md-align="center">
+          <div>
+            <div v-bind:class="{ 'clickable' : ticTacToe.IsMoveValid(3 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(0)" >
+              <div>
+                {{ticTacToe.Playground[0]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(3 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(3)" >
+              <div>
+              {{ticTacToe.Playground[3]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(6 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(6)" >
+              <div>
+              {{ticTacToe.Playground[6]}}
+              </div>
+            </div>
           </div>
-        </button>
+          <div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(1 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(1)" >
+              <div>
+              {{ticTacToe.Playground[1]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(4 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(4)" >
+              <div>
+              {{ticTacToe.Playground[4]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(7 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(7)" >
+              <div>
+              {{ticTacToe.Playground[7]}}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(2 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(2)" >
+              <div>
+              {{ticTacToe.Playground[2]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(5 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(5)" >
+              <div>
+              {{ticTacToe.Playground[5]}}
+              </div>
+            </div>
+            <div :class="{ 'clickable' : ticTacToe.IsMoveValid(8 , 'X') }" class="field md-whiteframe-3dp" @click="fieldPressed(8)" >
+              <div>
+              {{ticTacToe.Playground[8]}}
+              </div>
+            </div>
+          </div>
+        </md-layout>
       </md-layout>
+
     </md-layout>
-
-
   </md-layout>
 </template>
 
 <script>
-import Client from './classes/Client.js'
+import Client from './classes/Client.js';
+import TicTacToe from './classes/TicTacToe.js';
 
-const data = {
-  client : new Client()
-}
 
 export default {
   name: 'app',
   data () {
-    return data
+    return {
+      client : new Client(),
+      ticTacToe : new TicTacToe(),
+      inQue : false,
+      playernames : [],
+      playername : '',
+      enemyname : '',
+      chatmessages : [],
+      enemyFound : false,
+      wonPlayername : '',
+      games : [
+        {
+          moves : 3,
+          enemyname : 'Niklas',
+          won: false
+        },
+        {
+          moves : 3,
+          enemyname : 'Niklas',
+          won: true
+        },
+        {
+          moves : 3,
+          enemyname : 'Niklas',
+          won: true
+        }
+      ]
+    }
   },
   methods : {
     toggleSidenav() {
@@ -88,17 +185,42 @@ export default {
         returnMsg = "won";
       }
       return returnMsg;
+    },
+    fieldPressed ( fieldNr )   {
+      console.log(" Field " + fieldNr + " pressed. ");
+
+      if(this.ticTacToe.IsMoveValid( fieldNr , 'X'))
+      {
+        this.client.SendMove ( this.playername , this.enemyname , fieldNr );
+        this.ticTacToe.SetField( fieldNr , 'X' );
+
+        console.log("Clicked " + fieldNr , this.ticTacToe.Playground);
+        this.ticTacToe.Playground.__ob__.dep.notify();
+      }
+    },
+    handleSearchGamePressed() {
+      console.log("adnjlodjals");
+      this.inQue=true;
+      this.client.AddPlayer(this.playername);
+    },
+
+    fakeEnemyFound() {
+      this.inQue = false;
+      this.enemyFound = true;
+      this.enemyname = 'Kuruneko';
     }
   },
   computed: {
-    reversedUsername() {
-      return this.client.PlayerUser.split('').reverse().join('')
-    }
+
   }
 }
 </script>
 
 <style lang="scss">
+  $midnight-blue: #2c3e50;
+  $clouds : #ecf0f1;
+  $primary: #303F9F;
+
   .lost {
     color: #E91E63 !important;
   }
@@ -110,6 +232,7 @@ export default {
   #searchbox {
     margin: 50px auto auto auto;
     padding: 8px;
+    margin-bottom: 50px;
 
     & .md-card-content {
       padding: 8px;
@@ -121,5 +244,80 @@ export default {
       }
     }
   }
+  #playground {
+    & .field {
+      background-color: $primary;
+      height: 100px;
+      width: 100px;
+      margin: 5px;
+      cursor: default;
+
+      transition: all 1s ease-in-out;
+
+      & > div {
+        height: 100%;
+        width: 100%;
+        text-align: center;
+        line-height: 100px;
+        vertical-align: middle;
+        font-size: 62px;
+        background-color: $clouds; /*.mdl-color--indigo-700*/
+        color: transparent;
+        text-shadow: 0 0px 12px rgba(0,0,0,.05);
+        /*filter: drop-shadow();*/
+        -webkit-background-clip: text;
+        -moz-background-clip: text;
+        background-clip: text;
+      }
+
+      @media (min-width: 1000px) {
+          height: 150px;
+          width: 150px;
+
+          & > div {
+            font-size: 87px;
+            line-height: 150px;
+          }
+        }
+    }
+  }
+
+  #loader {
+    align-items: center;
+  }
+
+  .clickable {
+    cursor: pointer !important;
+  }
+
+  .main-content {
+    min-height: 100%;
+  }
+
+  .md-button {
+    padding: 0;
+
+    & > div {
+      padding: 0 16px;
+    }
+  }
+
+  #MatchPlayers {
+    margin-bottom: 50px;
+    margin-top: 50px;
+
+    @media (max-width: 500px) {
+      margin-bottom: 20px;
+      margin-top: 10px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .md-display-2 {
+      font-size: 30px;
+    }
+  }
+
+
 
 </style>
